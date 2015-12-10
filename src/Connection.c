02c4649324df591ab805a433547ec5f1218665fe
Gabriel <colecionador.gabriel@gmail.com>
2015-12-10 12:55:42 -0200

    Fix =

--- src/Connection.c
+++ src/Connection.c
@@ -1344,7 +1344,7 @@ void handle_message(char *buffer, int handle_internal){
 				}
 				else {
 					pointer += 2;
-					life = (int) = *pointer;
+					life = (int) *pointer;
 					//Update alive status
 					if(life <= 0) {
 						clients[i].alive = 0;
